af2dad3c6754f25ba50204018c417a1d8dcb63bd
AlinaRakhimova <rr.alina@yandex.ru>
2018-05-26 23:06:30 +0300

    ОБъединение с Main

--- Animal.java
+++ Animal.java
@@ -1,6 +1,26 @@
 package com.company;
 
-public class Animal {
+class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Cat cat1 = new Cat();
+        cat1.jump(2);
+        cat1.run(100);
+        cat1.run(500);
+        cat1.swim(200);
+        System.out.println("");
+        Dog dog1 = new Dog();
+        dog1.swim(10);
+        dog1.jump(1.5);
+        dog1.run(1000);
+        dog1.run(200);
+
+    }
+
+}
+
+ class Animal {
 
     public void run(double par) {
         System.out.println("Run, " + par);
